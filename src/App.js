1e1f0dd62b40d70385b288b740ac11088f618576
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2019-09-27 11:07:40 -0300

    Header creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Sid Surf Store</h1>
-        </div>
+        <BrowserRouter>
+            <Header />
+            <Routes />
+            <GlobalStyle />
+        </BrowserRouter>
     );
 }
 
