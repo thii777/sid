1d26452602ce0684312bef2e4443c3040b4536db
Thiago Carvalho <thiagocarvalho.ads@gmail.com>
2019-10-17 09:32:05 -0300

    Display product quantity on purchase button

--- src/pages/Home/Shoes/index.js
+++ src/pages/Home/Shoes/index.js
@@ -32,6 +32,7 @@ class Shoes extends Component {
 
     render() {
         const { products } = this.state;
+        const { amount } = this.props;
 
         return (
             <ProductList>
@@ -45,7 +46,8 @@ class Shoes extends Component {
                             onClick={() => this.handleAddProduct(product)}
                         >
                             <div>
-                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                                {amount[product.id] || 0}
                             </div>
                             <span>adicionar ao carrinho</span>
                         </button>
@@ -56,10 +58,18 @@ class Shoes extends Component {
     }
 }
 
+const mapStateToProps = state => ({
+    amount: state.cart.reduce((amount, product) => {
+        amount[product.id] = product.amount;
+
+        return amount;
+    }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
     bindActionCreators(CartActions, dispatch);
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(Shoes);
